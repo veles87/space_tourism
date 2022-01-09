14a4100dbb6f96ad305d171740508423419a0131
Maksim Chekhovskiy <veles.cafe@gmail.com>
2022-01-09 21:50:37 +0200

    m

--- js/main.js
+++ js/main.js
@@ -3,5 +3,7 @@ $(function(){
         prevArrow: '<img class="slider__arrow slider__arrow-left" src="./images/arrows/arrow-left.svg" alt="left-arrow">',
         nextArrow: '<img class="slider__arrow slider__arrow-right" src="./images/arrows/arrow-right.svg" alt="right-arrow">'
     });
-    
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').toggleClass('active');
+    } );
 });
