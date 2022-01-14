90571d7250cf4b7583cdb11c4cfe0108b2e47c9f
Maksim Chekhovskiy <veles.cafe@gmail.com>
2022-01-15 01:31:33 +0200

    done @media and all site

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 $(function(){
     $('.slider__box').slick({
         prevArrow: '<img class="slider__arrow slider__arrow-left" src="./images/arrows/arrow-left.svg" alt="left-arrow">',
-        nextArrow: '<img class="slider__arrow slider__arrow-right" src="./images/arrows/arrow-right.svg" alt="right-arrow">'
+        nextArrow: '<img class="slider__arrow slider__arrow-right" src="./images/arrows/arrow-right.svg" alt="right-arrow">',
+        responsive: [
+    {
+      breakpoint: 561,
+      settings: {
+        arrows: false
+      }
+    }
+  ]
     });
     $('.menu__btn').on('click', function () {
         $('.menu__list').toggleClass('active');
